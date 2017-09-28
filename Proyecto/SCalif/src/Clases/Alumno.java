3e21288071c59d5402a791948c004efaae14b5f0
RubiRojas <rojas.rubi@hotmail.com>
2017-09-28 18:59:51 -0500

    Se agrego la clase Alumno

---
+++ Proyecto/SCalif/src/Clases/Alumno.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Usuario lenovo
+ */
+public class Alumno {
+   private int idAlumno;
+   private String nombre;
+   private String NC;
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNC() {
+        return NC;
+    }
+
+    public void setNC(String NC) {
+        this.NC = NC;
+    }
+   
+   
+   
+   
+}
