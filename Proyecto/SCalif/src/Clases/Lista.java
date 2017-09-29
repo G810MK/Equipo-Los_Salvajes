792fba806a8fe9458e9c3e5737cad9a177cb7f83
Miguel <cc210a@gmail.com>
2017-09-28 19:12:59 -0500

    Se agrego la clase Lista

---
+++ Proyecto/SCalif/src/Clases/Lista.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Clases;
+
+/**
+ * 
+ * @author MHC <miguel.hrc>
+ * @date 28-sep-2017
+ * @time 19:10:54   
+ */
+public class Lista {
+    private int idLista;
+    private String Materia;
+    private String Grupo;
+    private int Semestre;
+
+    public int getIdLista() {
+        return idLista;
+    }
+
+    public void setIdLista(int idLista) {
+        this.idLista = idLista;
+    }
+
+    public String getMateria() {
+        return Materia;
+    }
+
+    public void setMateria(String Materia) {
+        this.Materia = Materia;
+    }
+
+    public String getGrupo() {
+        return Grupo;
+    }
+
+    public void setGrupo(String Grupo) {
+        this.Grupo = Grupo;
+    }
+
+    public int getSemestre() {
+        return Semestre;
+    }
+
+    public void setSemestre(int Semestre) {
+        this.Semestre = Semestre;
+    }
+    
+    
+
+}
